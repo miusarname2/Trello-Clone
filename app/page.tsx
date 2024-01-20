4061ce7ff8a753592ff24b9df06da4468074a706
miusarname <oscarm.alvarezg@gmail.com>
2024-01-20 00:27:04 -0500

    fix: :bug: fix an stupid bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-"use client"
+"use client";
+export default function Page() {
+  // Verificar si estamos en el lado del cliente
+  if (process.browser) {
+    // Código que solo se ejecuta en el navegador
+    window.location.href = "/login";
+  }
 
-export default function Home() {
-  window.location.href="/login"
+  // El resto del componente o código que se ejecutará en el servidor durante la construcción
+  // Puedes dejarlo vacío si no hay nada más que hacer en el servidor
+
+  return <div>Contenido de la página</div>;
 }
+
