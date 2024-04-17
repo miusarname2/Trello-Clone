834b238bd6a3355442fd9d87e786b939d28da7ce
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2024-04-16 19:54:05 -0500

    feat: :sparkles: Added configurations to be able to use the database(s)

--- TrelloBackend/Program.cs
+++ TrelloBackend/Program.cs
@@ -1,4 +1,15 @@
+using TrelloBackend.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+const string ConnectionName = "MysqlServerConnection";
+var connectionString = builder.Configuration.GetConnectionString(ConnectionName);
+
+// Add Context
+
+builder.Services.AddDbContext<TrelloDbContext>(options => options.UseSqlServer(connectionString));
 
 // Add services to the container.
 
