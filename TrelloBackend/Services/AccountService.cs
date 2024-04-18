c3dc8df22df5020c11713155676c84130f2a3275
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2024-04-18 09:40:35 -0500

    feat: :sparkles: The services were created

---
+++ TrelloBackend/Services/AccountService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrelloBackend.DataAccess;
+using TrelloBackend.Model.DataModel;
+using TrelloBackend.Model.InputDataModel;
+
+namespace TrelloBackend.Services
+{
+    public class AccountService : IAccountService
+    {
+        public async Task<bool> LogIn(UserLogin userData, TrelloDbContext _context)
+        {
+            var UserFinded = await _context.Users.FirstOrDefaultAsync(u=>u.Email == userData.Email && u.Password == userData.Password);
+            if (UserFinded != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<User> Register(TrelloDbContext _context, User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+    }
+}
---
+++ TrelloBackend/Services/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using TrelloBackend.DataAccess;
+using TrelloBackend.Model.DataModel;
+using TrelloBackend.Model.InputDataModel;
+
+namespace TrelloBackend.Services
+{
+    public interface IAccountService
+    {
+        Task<bool> LogIn(UserLogin userData,TrelloDbContext _context);
+        Task<User> Register(TrelloDbContext _context,User user);
+    }
+}
