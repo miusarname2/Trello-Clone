9d5fb0bad55cc0b02298c1e5941f09c78f4f89a3
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2024-04-17 19:20:18 -0500

    feat: :sparkles: Controllers are created and modified in order to allow their optimal functioning.

---
+++ TrelloBackend/Controller/ColumnsController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TrelloBackend.DataAccess;
+using TrelloBackend.Model.DataModel;
+
+namespace TrelloBackend.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColumnsController : ControllerBase
+    {
+        private readonly TrelloDbContext _context;
+
+        public ColumnsController(TrelloDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Columns
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Column>>> GetColumns()
+        {
+            return await _context.Columns.Include(u => u.Tasks).ToListAsync();
+        }
+
+        // GET: api/Columns/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Column>> GetColumn(int id)
+        {
+            var column = await _context.Columns.FindAsync(id);
+
+            if (column == null)
+            {
+                return NotFound();
+            }
+
+            return column;
+        }
+
+        // PUT: api/Columns/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutColumn(int id, Column column)
+        {
+            if (id != column.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(column).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ColumnExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Columns
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Column>> PostColumn(Column column)
+        {
+            var tableToModify = await _context.Tables.FindAsync(column.TableId);
+            if (tableToModify != null)
+            {
+                tableToModify.Columns.Add(column);
+            }
+            _context.Columns.Add(column);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetColumn", new { id = column.Id }, column);
+        }
+
+        // DELETE: api/Columns/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteColumn(int id)
+        {
+            var column = await _context.Columns.FindAsync(id);
+            if (column == null)
+            {
+                return NotFound();
+            }
+
+            _context.Columns.Remove(column);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ColumnExists(int id)
+        {
+            return _context.Columns.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ TrelloBackend/Controller/TablesController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TrelloBackend.DataAccess;
+using TrelloBackend.Model.DataModel;
+
+namespace TrelloBackend.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TablesController : ControllerBase
+    {
+        private readonly TrelloDbContext _context;
+
+        public TablesController(TrelloDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tables
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Table>>> GetTables()
+        {
+            return await _context.Tables.Include(u => u.Columns).ToListAsync();
+        }
+
+        // GET: api/Tables/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Table>> GetTable(int id)
+        {
+            var table = await _context.Tables.FindAsync(id);
+
+            if (table == null)
+            {
+                return NotFound();
+            }
+
+            return table;
+        }
+
+        // PUT: api/Tables/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTable(int id, Table table)
+        {
+            if (id != table.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(table).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TableExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tables
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Table>> PostTable(Table table)
+        {
+            var UserToModify = await _context.Users.FindAsync(table.UserId);
+
+            if (UserToModify != null)
+            {
+                UserToModify.Tables.Add(table);
+                // Imprimir las tablas en la consola antes de guardar cambios
+                Console.WriteLine("Lista de Tables antes de guardar cambios:");
+                foreach (var tables in UserToModify.Tables)
+                {
+                    Console.WriteLine($"ID: {tables.Id}, Nombre: {tables.Name}");
+                }
+            }
+
+            _context.Tables.Add(table);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTable", new { id = table.Id }, table);
+        }
+
+        // DELETE: api/Tables/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTable(int id)
+        {
+            var table = await _context.Tables.FindAsync(id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tables.Remove(table);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TableExists(int id)
+        {
+            return _context.Tables.Any(e => e.Id == id);
+        }
+    }
+}
