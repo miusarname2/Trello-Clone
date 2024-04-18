f8961d00a0a2fb9b2955f45f8ec52a8f468e5900
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2024-04-18 09:41:42 -0500

    feat: :sparkles: The UserLogin model was created

---
+++ TrelloBackend/Model/InputDataModel/UserLogin.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrelloBackend.Model.InputDataModel
+{
+    public class UserLogin
+    {
+        [Required,EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
